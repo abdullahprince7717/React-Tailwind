034cb7a44b36a6ebeb922720966b550dca341c29
abdullahprince7717 <abdullahprince7717@gmail.com>
2024-02-10 03:03:37 +0500

    fixed navbar responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,13 +15,13 @@ function Navbar() {
     return (
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
             <h1 className='w-full text-3xl font-bold text-[#00df9a]'>React.</h1>
-            <ul className='flex hidden'>
+            <ul className='flex hidden sm:flex '>
                 <a className='p-4' href='#'>Home</a>
                 <a className='p-4' href='#'>About</a>
                 <a className='p-4' href='#'>Services</a>
                 <a className='p-4' href='#'>Contact</a>
             </ul>
-            <div onClick={handleMenu}>
+            <div onClick={handleMenu} className="sm:hidden">
                 {showMenu ? <GiHamburgerMenu size={20} /> : <IoMdClose size={20} />}
             </div>
             <div className={!showMenu ? "fixed top-0 w-[60%] h-full ease-in-out duration-500" : "hidden"}>
